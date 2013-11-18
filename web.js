c45c33feacf7a975c429df22171e1cd297cdd1ae
orion <orionzhao91@gmail.com>
2013-11-17 22:39:11 -0800

    finished two buttons

--- web.js
+++ web.js
@@ -8,35 +8,40 @@ var express = require('express')
 
 var app = express()
 
+/*
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib())
-}
+}*/
 
-/*var engines = require('consolidate');
-app.engine('html', engines.swig); */
+var engines = require('consolidate');
+app.engine('html', engines.swig);
 
 app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
+app.set('view engine', 'html')
 app.use(express.logger('dev'))
+
+/*
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
 ))
+*/
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.render('index',
-  	{title : 'A hungry fool'}
-  	)
+  res.render('index')
 })
 
 app.get('/recent', function (req, res) {
-  res.render('recent',
-    {title : 'Foolish Things'}
-    )
+  res.render('recent')
+})
+
+app.get('/a-stack-a-day-keeps-the-bug-away', function (req, res) {
+  res.render('a-stack-a-day-keeps-the-bug-away')
 })
 
 var port = process.env.PORT || 3000
