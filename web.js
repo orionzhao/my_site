e014ca96822d66c8717965c866b03b4e4425e4de
Orion Zhao <orion@relayrides.com>
2014-04-14 01:37:58 -0700

    styling update

--- web.js
+++ web.js
@@ -10,12 +10,11 @@ var express = require('express')
 
 var app = express()
 
-/*
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib())
-}*/
+}
 
 var engines = require('consolidate');
 app.engine('html', engines.swig);
@@ -27,13 +26,11 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'html')
 app.use(express.logger('dev'))
 
-/*
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
 ))
-*/
 
 app.use(express.static(__dirname + '/public'))
 
